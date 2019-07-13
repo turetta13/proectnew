d306fd6cd3edbfca8120abb775fb93724a99b5ef
turetta13 <turetta13@gmail.com>
2019-07-13 16:53:47 +0300

    updt

---
+++ storage/framework/views/790b27f46dcd7b77d52a11d6bdfb7ea85b326179.php
@@ -0,0 +1,283 @@
+<?php $__env->startSection('content'); ?>
+
+       <!--//header-->
+    <!--banner-->
+    <div class="banner">
+        <div class="container">
+            <h2 class="hdng"><span>S.Baker</span> with love</h2>
+            <p>Наши торты сделают ваш день особенным</p>
+            <a href="<?php echo e(route('products')); ?>">Купить сейчас</a>
+            <div class="banner-text">
+                <img src="images/2.png" alt=""/>
+            </div>
+        </div>
+    </div>
+    <!--//banner-->
+    <!--gallery-->
+    <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $key): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <div class="gallery">
+        <div class="container">
+            <div class="gallery-grids">
+                <div class="col-md-8 gallery-grid glry-one">
+                    <a href="/prod"><img src="<?php echo e($key->image_path); ?>" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">подробнее</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p><?php echo e($key->name); ?></p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price"><?php echo e($key->price); ?></h5>
+                            </div>
+
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                <div class="col-md-4 gallery-grid glry-two">
+                    <a href="/prod"><img src="images/g2.jpg" class="img-responsive" alt=""/>
+                        <div class="gallery-info galrr-info-two">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+             
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g3.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g4.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g5.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g6.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g7.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g8.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g9.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 gallery-grid ">
+                    <a href="/prod"><img src="images/g10.png" class="img-responsive" alt=""/>
+                        <div class="gallery-info">
+                            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> view</p>
+                            <a class="shop" href="/single">SHOP NOW</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                    <div class="galy-info">
+                        <p>Lorem Ipsum is simply</p>
+                        <div class="galry">
+                            <div class="prices">
+                                <h5 class="item_price">$95.00</h5>
+                            </div>
+                            <div class="rating">
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                                <span>☆</span>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+       <?php $__env->stopSection(); ?>
+
+    <?php $__env->startSection('footer'); ?>
+       <?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layout.main', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
